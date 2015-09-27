c5a41a25e1a2e22b7f02cc12b81214b15459faa0
Brett Bush <brettjbush@gmail.com>
2015-09-27 11:50:23 -0500

    init commit

---
+++ Solfeggio/src/solfeggio/MusicGenerator.java
@@ -0,0 +1,25 @@
+package solfeggio;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Brett Bush
+ */
+public class MusicGenerator {
+    
+    // Constructor
+    public MusicGenerator()
+    {
+        
+    }
+    
+    public void generate()
+    {
+        
+    }
+    
+}
